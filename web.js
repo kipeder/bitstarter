d6f07977f1ce3217427d26675ad71b3fefca5644
Kip Eder <kip.eder@comcast.net>
2013-07-25 18:50:54 +0000

    Updated with Hello from index.html

--- web.js
+++ web.js
@@ -1,9 +1,42 @@
+#!/usr/bin/env node
+var fs = require('fs');
 var express = require('express');
 var app = express();
+
+//var helloworld = function() {
+    var txt = "index.html";
+    var yyy = fs.readFileSync(txt); 
+//var xxx = function(txt) {
+//    var yyy = new Buffer();
+console.log("  yyy = " + yyy);
+
+console.log("  yyy = 2");
+
+//	var zzz = buf.toString();
+//	return zzz;
+
+//};
+//	if (err) throw err;
+
+//	var hellobuf = buf.toString();
+
+//	console.log(xxx);
+
+//   return data;
+//};
+
+
+//console.log("  yyy = " + yyy);
+//var zzz = function() { buf.toString();};
+//var but = new Buffer(yyy);
+//var zzz = buf.toString();
+
+//console.log("  zzz = " + zzz);
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(yyy);
 });
 
 var port = process.env.PORT || 5000;
