6b4d18480b96be3ec134d7fb5ac86da1b947b599
Kip Eder <kip.eder@comcast.net>
2013-07-26 18:29:11 +0000

    Final Hello World from index.html

--- web.js
+++ web.js
@@ -3,40 +3,21 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-//var helloworld = function() {
-    var txt = "index.html";
-    var yyy = fs.readFileSync(txt); 
-//var xxx = function(txt) {
-//    var yyy = new Buffer();
-console.log("  yyy = " + yyy);
 
-console.log("  yyy = 2");
+var txt = "index.html";
 
-//	var zzz = buf.toString();
-//	return zzz;
+var yyy = fs.readFileSync(txt); 
 
-//};
-//	if (err) throw err;
-
-//	var hellobuf = buf.toString();
-
-//	console.log(xxx);
-
-//   return data;
-//};
-
+var zzz = yyy.toString();
 
 //console.log("  yyy = " + yyy);
-//var zzz = function() { buf.toString();};
-//var but = new Buffer(yyy);
-//var zzz = buf.toString();
 
 //console.log("  zzz = " + zzz);
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(yyy);
+  response.send(zzz);
 });
 
 var port = process.env.PORT || 5000;
